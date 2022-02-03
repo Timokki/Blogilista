7e3e128a57a8f267d7ffa52a178bc41668bbaab4
Timo Keränen <timo@timokeranen.com>
2022-02-03 19:53:24 +0200

    Turhia kommentteja poistettu

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const app = require('./app') // varsinainen Express-sovellus
 const http = require('http')
 const envs = require('./utils/config')
-//const logger = require('./utils/logger')
 
 const server = http.createServer(app)
-
-/*server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})*/
 
 server.listen(envs.PORT, () => {
   console.log(`Server running on port ${envs.PORT}`)
