22f14f5743f0e36090967148b7277d53d0cfbbab
Timo Keränen <timo@timokeranen.com>
2022-04-04 18:58:07 +0300

    GET api/blogs testi toimii. 4.8 step 1

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,11 @@
 const procEnvs = require('dotenv').config()
 
-const MONGODB_URI = procEnvs.parsed.MONGODB_URI
+
+const MONGODB_URI = procEnvs.parsed.NODE_ENV === 'test'   
+? procEnvs.parsed.TEST_MONGODB_URI  
+: procEnvs.parsed.MONGODB_URI
+
+//const MONGODB_URI = procEnvs.parsed.MONGODB_URI
 const PORT = procEnvs.parsed.PORT
 
 module.exports = {
